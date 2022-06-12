fccfe4a08cdf5c0d52276b37f72af2322c132775
tearheart <travis.earheart@gmail.com>
2022-06-12 21:24:25 +0000

    Add GitHub Actions

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 import logging
 from logging.config import dictConfig
-from log_config import log_config # this is your local file
+from src.log_config import log_config # this is your local file
 
 dictConfig(log_config)
 logger = logging.getLogger("polygonio-news-sentiment-logger")
